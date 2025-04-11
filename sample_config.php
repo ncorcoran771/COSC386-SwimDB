933b56a9b7957bed4933fd1028fb345c2603a5c9
ncorcoran771 <165853101+ncorcoran771@users.noreply.github.com>
2025-04-11 10:36:54 -0400

    Update sample_config.php

--- sample_config.php
+++ sample_config.php
@@ -1,1 +1,17 @@
+<?php
+// config.php
 
+// Database configuration
+$host = 'localhost';        // or the IP address of your MySQL server
+$dbname = 'your_database_name';
+$username = 'your_database_user';
+$password = 'your_database_password';
+
+// Create connection
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
