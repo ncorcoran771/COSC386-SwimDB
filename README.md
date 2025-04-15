6a36f31358597dbdefff8c1c223c7042463bbbe1
Thaddeus <thaddeusversteegen@gmail.com>
2025-04-15 13:06:41 -0400

    finished the login portion

--- README.md
+++ README.md
@@ -5,16 +5,21 @@ I'm using this as our todo / notes area
 
 TODO:
 - [ ] password hashes
-- [ ] login verification
+- [x] login verification
 - [ ] GUI fluffing
 - [ ] file path clarifications
 - [ ] a standardized/centralized place to hand sql queries?
 - [ ] finalize ER diagram and database schema
+- [ ] look into Prepared Statements as injection resistance: https://www.php.net/manual/en/mysqli.quickstart.prepared-statements.php sounds cool & handy in general 
 
 other notes:
 1. I'm running under the assumption that database.sql is the file we're using to create the database tables. if it's not please tell me and/or move them somewhere else
 2. we cannot have login / connection details stored in public_html
 3. how are we storing users/passwords? if it's on their individual tables like in the diagram or a separate users table. login.php assumes individual tables.
+4. Recommended VSCode extensions (if you use it):
+    - devsense.phptools-vscode
+    - xdebug.php-debug
+
 
 W3 pages that were helpful (please add to list)
 1. Sessions: https://www.w3schools.com/php/php_sessions.asp
@@ -24,5 +29,5 @@ Session Variables:
 | ---- | ----- | ----------- |
 | user | users ID | int |
 | userType | users type | 'swimmer' 'coach' 'admin' |
-| loggedIN | whether they've successfully logged in | 1 if verified |
+| loggedIN | login success | true if verified |
     
--- login.php
+++ login.php
@@ -7,35 +7,33 @@
     unset($plainPass);
 
 //sets the user type as a session variable
-    if(isset(_POST['swimmerLog'])) // if swimmer button
+    if(isset($_POST['swimmerLog'])) // if swimmer button
         $_SESSION['userType'] = 'swimmer';
-    else if(isset(_POST['coachLog'])) // if coach button
+    else if(isset($_POST['coachLog'])) // if coach button
         $_SESSION['userType'] = 'coach';
-    else if(isset(_POST['adminLog'])) // if admin button
+    else if(isset($_POST['adminLog'])) // if admin button
         $_SESSION['userType'] = 'admin';
     else
-        echo "SOMETHING WENT HORRBLY WRONG!!"; //fail case...
+        echo "SOMETHING WENT HORRBLY WRONG!!\n"; //fail case...
 
 //sets the users ID in the session variable
     $_SESSION['user'] = $userID;
 
-    $query = passQuery($_SESSION['userType'], $_SESSION['userID'], $hashPassword);
+    $output = mysqli_query($conn, passQuery($_SESSION['userType'], $_SESSION['user'], $hashPassword));
+    if(!$outpt)//returns false of failure
+        echo "SQL QUERY FAILED\n";
 
-    $user = mysqli_query($conn, $query);//we'd want to save some part of this as the users session variable?
+    $user = mysqli_fetch_assoc($output);
+    if(mysqli_fetch_assoc($output))//if another row exists we don messed up
+        echo "SQL QUERY RETURNED MULTIPLE ROWS \n";
 
-    //verifying the user goes here. I aint doin that right now
-    if(/*user is verified*/){
-
-        //redirect to their specific page?
-
-        $_SESSION['logedIN'] = 1;
+    if($user){//user exists and so much be who they say they are?
+        $_SESSION['logedIN'] = true; // they've logged in.
+        //do we want to save anything else from their query?
+        //maybe redirect to a home page?
     }
 
-
-
-
-
-    //password query building function
+    // password query building function
     // we want an entire tuple of one person, based on their password & id
     // technically possible to have same id same password different gender but lets ignore that
     function passQuery($type, $id, $hashPass){
