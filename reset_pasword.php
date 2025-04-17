8bdb858538a151dbc344ceaf2e9d398a26e57b69
duckydoe <ejscout1397@gmail.com>
2025-04-17 14:43:24 -0400

    Create reset_pasword.php

---
+++ reset_pasword.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+$conn = mysqli_connect("localhost", "username", "password", "swim_data");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$userID = $_POST['userID'] ?? '';
+if (empty($userID)) {
+    die("User ID is required.");
+}
+
+$tables = [
+    'Swimmer' => 'swimmerID',
+    'Coaches' => 'coachID',
+    'Administrator' => 'adminID'
+];
+
+$found = false;
+foreach ($tables as $table => $idField) {
+    $query = "SELECT * FROM $table WHERE $idField = '$userID'";
+    $result = mysqli_query($conn, $query);
+    if (mysqli_num_rows($result) === 1) {
+        $found = true;
+        $newPass = bin2hex(random_bytes(4)); // 8-char reset
+        $hashed = hash('sha256', $newPass);
+        $update = "UPDATE $table SET password = '$hashed' WHERE $idField = '$userID'";
+        mysqli_query($conn, $update);
+        echo "<h2>Password reset!</h2>";
+        echo "Your new password is: <strong>$newPass</strong><br>";
+        echo "<a href='indexp.php'>Return to Login</a>";
+        break;
+    }
+}
+if (!$found) {
+    echo "<h2>User ID not found.</h2>";
+    echo "<a href='indexp.php'>Return to Login</a>";
+}
+?>
