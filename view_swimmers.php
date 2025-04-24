15ddfcf5b988e37fd6be45d8d46cb1c3c4e6b29a
duckydoe <ejscout1397@gmail.com>
2025-04-24 12:46:04 -0400

    Update view_swimmers.php

--- view_swimmers.php
+++ view_swimmers.php
@@ -100,10 +100,11 @@ $result = mysqli_query($conn, $query);
             <tr><td colspan="6">No swimmers found.</td></tr>
         <?php endif; ?>
     </table>
-                       <div>
+    <div>
         <a href="home.php">Back to Home</a> | 
         <a href="logout.php">Logout</a>
     </div>
 </body>
 </html>
 <?php mysqli_close($conn); ?>
+
